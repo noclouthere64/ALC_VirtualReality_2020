59f42238aa70d9d5eaf5c998a8b774d3fb17df6a
Miguel Mendoza <miguel.m668@stu.nebo.edu>
2020-02-25 10:30:31 -0700

    Made an empty object to contain all the tree prefabs. Unzipped the vr_bbq world.

---
+++ MicroVRProject/Assets/Scripts/FoodItem.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodItem : MonoBehaviour
+{
+    public MeshRenderer mr;
+    public float cookRate;
+
+    private bool isCooking;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isCooking)
+            mr.material.color = Color.Lerp(mr.material.color, Color.black, cookRate * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if(collision.gameObject.CompareTag("CookSurface"))
+            isCooking = true;
+    }
+
+    private void OnTriggerExit(Collider collision)
+    {
+        if(collision.gameObject.CompareTag("CookSurface"))
+            isCooking = false;
+    }
+}
